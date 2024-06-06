bb99136db07416709a6c778f0816ce0d7d712d90
IflanAhamad <iflanahamad@gmail.com>
2024-06-06 23:24:37 +0530

    using local storage

--- app.js
+++ app.js
@@ -2,10 +2,21 @@ let button = document.getElementById('add')
 let todoList = document.getElementById ('todoList')
 let input = document.getElementById('input');
 
+
+//local storage,cookies
 let todos = [];
+window.onload = () => {
+    todos = JSON.parse(localStorage.getItem('todos')) || []
+    todos.forEach(todo=>addtodo(todo))
+}
+
+
 
 button.addEventListener('click',()=>{
     todos.push(input.value)
+
+    localStorage.setItem('todos', JSON.stringify(todos))
+
     addtodo(input.value)
     input.value=''
 })
@@ -15,6 +26,7 @@ function addtodo(todo){
    let para= document.createElement('p')
    para.innerText = todo;
    todoList.appendChild(para)
+
    para.addEventListener('click',() =>{
       para.style.textDecoration ='line-through'
       remove(todo)
@@ -32,4 +44,6 @@ function remove(todo){
     let index = todos.indexOf(todo)
     if(index>-1)
         todos.splice(index,1)
+
+    localStorage.setItem('todos', JSON.stringify(todos))
 }
